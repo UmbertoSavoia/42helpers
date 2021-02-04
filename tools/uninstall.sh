51d4ce58c7ad9f95bffde67e4c3e9cdab660eaaa
Leonardo Rocca <lrocca@student.42roma.it>
2021-02-04 19:35:54 +0100

    Better uninstall.sh - removes trailing newlines - tidier output - now greets the user :)

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -1,18 +1,23 @@
 #!/bin/zsh
 
-underline=`tput smul`
-nounderline=`tput rmul`
-bold=`tput bold`
 normal=`tput sgr0`
+bold=`tput bold`
 
 if [ -d ~/.42helpers ]
 then
 	echo "Deleting ${bold}42helpers${normal} folder..."
 	rm -rf ~/.42helpers
-	echo "Goodbye! (-_-;)"
+
+	echo "Removing startup command from ~/.zshrc..."
+	sed -i '' 's/zsh ~\/.42helpers\/tools\/rc.sh//g' ~/.zshrc
+
+	# Remove trailing spaces and newlines at the end of .zshrc
+	# https://stackoverflow.com/a/8635286
+	perl -e 'undef $/; $_ = <>; s/\s+\z/\n/; print' ~/.zshrc > ~/.zshrc-tmp
+	mv ~/.zshrc-tmp ~/.zshrc
+
+	echo "Goodbye ${USER}! (-_-;)"
 else
-	echo "${bold}42helpers${normal} is not installed. You can install it by following the instructions on this page:"
-	echo "> ${underline}https://github.com/UmbertoSavoia/42helpers${nounderline}"
-	echo ""
-	echo "If your terminal emulator supports it, you can open the link holding Cmd/Ctrl and clicking on it ;-)"
+	echo "${bold}42helpers${normal} is not installed."
+	exit 1
 fi
