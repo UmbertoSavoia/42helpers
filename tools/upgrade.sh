813d7fad017547c704513fde6b8e20c350476460
leeoocca <leeoocca@gmail.com>
2021-02-04 18:22:00 +0100

    Better upgrade script with prompt.

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -3,22 +3,37 @@
 bold=`tput bold`
 normal=`tput sgr0`
 
-current_path=`pwd`
+function upgrade_prompt {
+	while read -t -k 1 option; do true; done
+	[[ "$option" != ($'\n'|"") ]] && echo
+	echo -n "[42helpers] Would you like to upgrade? [Y/n] "
+	read -r -k 1 option
+	[[ "$option" != $'\n' ]] && echo
+	case "$option" in
+		[yY$'\n'])
+			echo "Upgrading ${bold}42helpers${normal}..."
+			git -C ~/.42helpers pull origin main --no-rebase
+			echo "Done :-)";;
+		[nN]) echo "Ok, maybe next time." ;;
+	esac
+}
 
 if [ -d ~/.42helpers ]
 then
-	cd ~/.42helpers
-	git fetch origin
-	if ! git diff --exit-code origin/main..main > /dev/null
+	if ! git -C ~/.42helpers fetch origin &> /dev/null
 	then
-		echo "Upgrading ${bold}42helpers${normal}..."
-		git pull origin main --no-rebase
-		echo "Done :-)"
+		echo "Couldn't check for upgrades."
+		echo "Check your Internet connection and try again."
+		exit 1
+	fi
+	if ! git -C ~/.42helpers diff --exit-code origin/main..main > /dev/null
+	then
+		upgrade_prompt
 	else
 		echo "${bold}42helpers${normal} is up to date."
 	fi
-		cd ${current_path}
 else
-	echo "${bold}42helpers${normal} is not installed."
+	echo "${bold}42helpers${normal} is not installed on your machine."
+	echo "Check the ~/.42helpers folder exists and try again."
 	exit 1
 fi
