b922e2ad18aa1f2277ae7756ea0500aa46dcb426
Leonardo Rocca <lrocca@student.42roma.it>
2021-02-08 15:22:05 +0100

    Added env checks to install.sh It asks the user for confirmation if installing on a non-42 machine

--- tools/install.sh
+++ tools/install.sh
@@ -1,36 +1,75 @@
 #!/bin/zsh
 
+# Define installation dir if undefined
+# ! [[ -v FTH ]] && FTH=~/.42helpers
+
+FTH=~/.42helpers
+
 normal=`tput sgr0`
 bold=`tput bold`
 
-# Installation dir
-FTH=~/.42helpers
+abort() {
+  printf "%s\n" "$1"
+  exit 1
+}
 
-# Should it check if var is already defined?
-# if [ -n "${FTH+1}" ]
-# then
-# 	FTH=~/.42helpers
-# fi
-
-if [ -d $FTH ]
-then
-	echo "${bold}42helpers${normal} is already installed."
-	exit 1
-else
+install() {
 	echo "Installing ${bold}42helpers${normal}..."
 	git clone https://github.com/UmbertoSavoia/42helpers.git ${FTH}
+
+	if ! grep -q "zsh ${FTH}/tools/rc.sh" ~/.zshrc; then
+		echo "Adding startup command to ~/.zshrc..."
+		echo "\nzsh $FTH/tools/rc.sh" >> ~/.zshrc
+	fi
+}
+
+install_prompt() {
+	while read -t -k 1 option; do true; done
+	[[ "$option" != ($'\n'|"") ]] && echo
+	echo -n "Are you sure you want to install ${bold}42helpers${normal}? [Y/n] "
+	read -r -k 1 option
+	[[ "$option" != $'\n' ]] && echo
+	case "$option" in
+		[yY$'\n'])
+			echo -n "Ok! "
+			install ;;
+		[nN]) exit ;;
+	esac
+}
+
+env_check() {
+	# First check OS
+	OS="$(uname)"
+	if [[ "$OS" == "Linux" ]]; then
+		echo "Some helpers may not work on Linux."
+		return 0
+	elif [[ "$OS" != "Darwin" ]]; then
+		echo "${bold}42helpers${normal} is only supported on macOS and Linux."
+		return 0
+	# Check 42 folders
+	elif ! [ -d /Library/Scripts/42 ] || ! [ -d /usr/share/norminette ] || ! [ -h ~/goinfre ]; then
+		echo "You aren't in a 42 cluster. Some helpers may not work on your machine."
+		return 0
+	else
+		return 1
+	fi
+}
+
+if ! [ -d $FTH ]
+then
+	if env_check; then
+		# Ask user for confirmation
+		install_prompt
+	else
+		# Everything looks fine. Conventional installation.
+		install
+	fi
+else
+	abort "${bold}42helpers${normal} is already installed."
 fi
 
-if ! grep -q "zsh ${FTH}/tools/rc.sh" ~/.zshrc
-then
-	echo "Adding startup command to ~/.zshrc..."
-	echo "\nzsh ~/.42helpers/tools/rc.sh" >> ~/.zshrc
-fi
-
-if [ -d $FTH ]
-then
+if [ -d $FTH ]; then
 	echo "Hooray! ${bold}42helpers${normal} has been installed."
 else
-	echo "An error occurred during the installation process."
-	exit 1
+	abort "An error occurred during the installation process."
 fi
